bef331f7ce97ba21b7f33859762d6b5dcd4098b1
MoonStorm <MoonStorm@gmail.com>
2015-10-15 22:52:08 -0600

    preparing the move to myget

--- Dapper.FastCRUD.ModelGenerator/Properties/AssemblyInfo.cs
+++ Dapper.FastCRUD.ModelGenerator/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Dapper.FastCrud")]
-[assembly: AssemblyDescription("A simple and fast wrapper for Dapper.")]
+[assembly: AssemblyTitle("Dapper.FastCRUD.ModelGenerator")]
+[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Dapper.FastCrud")]
-[assembly: AssemblyCopyright("Copyright © Dan Cristiu 2015")]
+[assembly: AssemblyProduct("Dapper.FastCRUD.ModelGenerator")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,13 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-#if NET_46_OR_GREATER
-[assembly: Guid("4df4976a-fb18-4466-ba8f-0a48e3f11b01")]  
-#else
-[assembly: Guid("0044043b-c8ec-497c-8b58-abe028014d24")]
-#endif
-
-[assembly:InternalsVisibleTo("Dapper.FastCrud.Tests")]
+[assembly: Guid("cca32482-6fd5-4841-bfb3-bb5eddf63301")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -38,6 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0")]
-// [assembly: AssemblyFileVersion("1.0.0.*")]
-
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
