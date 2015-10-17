9a1f0e0e520f887372b6117813243d4604699dc0
MoonStorm <MoonStorm@gmail.com>
2015-10-16 21:53:38 -0600

    build script fixed

--- MyGet.ps1
+++ MyGet.ps1
@@ -1,28 +1,13 @@
 ﻿$ErrorActionPreference = "Stop"
 
-param(
-    [string]$packageVersion,
-    [string]$configuration
-)
+$packageVersion = ($env:PackageVersion)
+$configuration = ($env:Configuration)
+$msBuildExe = ($env:MsBuildExe)
+$msBuildTarget = ($env:Targets)
 
-if(-not $packageVersion){
-    $packageVersion = ($env:PackageVersion)
-}
+& "$PSScriptRoot\.nuget\nuget.exe" restore "$PSScriptRoot\Dapper.FastCRUD.sln"
 
-if(-not $configuration){
-    $configuration = ($env:Configuration)
-}
+& "$msBuildExe" "$PSScriptRoot\Dapper.FastCRUD.sln" /t:"$msBuildTarget" /p:Configuration="$configuration"
 
-if(-not $packageVersion){
-    throw "invalid package version"
-}
-if(-not $configuration){
-    throw "invalid configuration"
-}
-
-"$PSScriptRoot\.nuget\nuget.exe" restore "$PSScriptRoot\Dapper.FastCRUD.sln"
-
-"$(${env:ProgramFiles(x86)})\MSBuild\14.0\Bin\MsBuild.exe" "$PSScriptRoot\Dapper.FastCRUD.sln" /t:Rebuild /p:Configuration="$configuration"
-
-"$PSScriptRoot\.nuget\nuget.exe" pack "$PSScriptRoot\NuGetSpecs\Dapper.FastCRUD.nuspec" -OutputDirectory Releases -Version "$packageVersion" -Properties configuration=$configuration -Verbosity detailed
-"$PSScriptRoot\.nuget\nuget.exe" pack "$PSScriptRoot\NuGetSpecs\Dapper.FastCRUD.ModelGenerator.nuspec" -OutputDirectory Releases -Verbosity detailed  -Version "$packageVersion" -Properties configuration=$configuration
+& "$PSScriptRoot\.nuget\nuget.exe" pack "$PSScriptRoot\NuGetSpecs\Dapper.FastCRUD.nuspec" -OutputDirectory Releases -Version "$packageVersion" -Properties configuration="$configuration" -Verbosity detailed
+& "$PSScriptRoot\.nuget\nuget.exe" pack "$PSScriptRoot\NuGetSpecs\Dapper.FastCRUD.ModelGenerator.nuspec" -OutputDirectory Releases -Verbosity detailed  -Version "$packageVersion" -Properties configuration="$configuration"
