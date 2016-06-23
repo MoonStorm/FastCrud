4dcbb6cf77fd6e91f1fb10f56ceb7cf1d29f65be
MoonStorm <MoonStorm@gmail.com>
2016-06-23 15:29:23 -0600

    project.json

--- MyGet.ps1
+++ MyGet.ps1
@@ -17,13 +17,13 @@ if ($LASTEXITCODE -ne 0){
 
 # & "$PSScriptRoot\.nunit\nunit-console.exe" "$PSScriptRoot\Dapper.FastCRUD.Tests\bin\$configuration\Dapper.FastCrud.Tests.dll" /exclude=ExternalDatabase /noshadow  /framework:v4.5
 # if ($LASTEXITCODE -ne 0){
-    # throw "tests failed"
+#     throw "tests failed"
 # }
 
-& "$PSScriptRoot\.nuget\nuget.exe" pack "$PSScriptRoot\NuGetSpecs\Dapper.FastCRUD.nuspec" -OutputDirectory Releases -Version "$packageVersion" -Properties configuration="$configuration" -Verbosity detailed
-if ($LASTEXITCODE -ne 0){
-    throw "packing failed"
-}
+#& "$PSScriptRoot\.nuget\nuget.exe" pack "$PSScriptRoot\NuGetSpecs\Dapper.FastCRUD.nuspec" -OutputDirectory Releases -Version "$packageVersion" -Properties configuration="$configuration" -Verbosity detailed
+#if ($LASTEXITCODE -ne 0){
+#    throw "packing failed"
+#}
 
 & "$PSScriptRoot\.nuget\nuget.exe" pack "$PSScriptRoot\NuGetSpecs\Dapper.FastCRUD.ModelGenerator.nuspec" -OutputDirectory Releases -Verbosity detailed  -Version "$packageVersion" -Properties configuration="$configuration"
 if ($LASTEXITCODE -ne 0){
