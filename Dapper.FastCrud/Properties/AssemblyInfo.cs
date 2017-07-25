1478f373783b0c2b48b0f51a85e709959121de35
MoonStorm <MoonStorm@gmail.com>
2017-07-25 00:58:43 -0600

    nuspec files deprecated

--- Dapper.FastCrud/Properties/AssemblyInfo.cs
+++ Dapper.FastCrud/Properties/AssemblyInfo.cs
@@ -14,8 +14,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("2.1.0.0")]
-[assembly: AssemblyFileVersion("2.1.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
